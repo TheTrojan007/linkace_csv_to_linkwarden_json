d342ca1a5432325b8d7b4f86166c7178a050b7c1
TheTrojan007 <gitea@wilkmail.de>
2024-02-23 21:57:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# linkace_csv_to_linkwarden_json
+
+Simple python script to convert a [LinkAce](https://www.linkace.org/)[(GitHub)](https://github.com/Kovah/LinkAce/) csv export file into a json file to import with [Linkwarden](https://linkwarden.app/)[(GitHub)](https://github.com/linkwarden/linkwarden)
+
+## Description
+
+The script reads all lines from the csv file using the basic csv python library. They are then converted into the respective dataclasses for the Linkwarden json format.
+Using the basic json library, these are then dumped into a json file ready for import to Linkwarden.
+Basic data needs to be provided in a file, the default is 'user_inputs.txt'. Minimal data is at least name and username. More can be provided as seen in the default file. Line can be commented with #.
+The datastructure is slightly different between the two application formats. Mainly, in LinkAce links can be part of multiple lists, but in Linkwarden the can only be part of one collection. Furthermore in LinkAce there is no need for a link to be part of a list, but in Linkwarden every link needs to be part of a collection.
+This leads to some loss of information if links are associated with multiple list in LinkAce. The script will convert all lists into collection but links will only be added to the last collection they are associated with (they could possibly be added to all collections instead, but that results in a lot of duplicate links).
+But tags work just the same in both applications, so no issues there.
+
+## Getting Started
+
+### Dependencies
+
+* Python with csv, json, string, dataclass and datetime libraries
+* Version 3.12.0 has all of them built in
+
+### Installing
+
+* Clone the git repository or download converter.py and user_inputs.txt
+
+### Executing program
+
+* In LinkAce use the 'Export to CSV' tool (Click the username on the top right > Export > Export to CSV)
+* Copy your LinkAce_export.csv file to the same folder as the script or change 'linkaceFilename' in the script to your file path
+* Change data in the user_inputs file
+* Run the script
+* File named backup.json is created
+* Import backup.json into Linkwarden (Settings > Import From > From Linkwarden)
+
+## Authors
+
+Paul Wilk
+[Mail](mailto:opensource@wilkmail.de)
+
+## Version History
+
+* 1.0
+    * Initial Release
---
+++ converter.py
@@ -0,0 +1,152 @@
+import csv
+import json
+import string
+from dataclasses import dataclass
+from dataclasses import field
+from datetime import datetime, timezone
+
+def read_user_inputs(file_path):
+     with open(file_path, 'r') as input_file:
+          user_inputs = {}
+          for line in input_file:
+               if line.startswith('#'):
+                    continue
+               else:
+                    key, value = line.translate({ord(c): None for c in string.whitespace}).split(':', 1)
+                    if value.lower() == 'true':
+                         value = True
+                    elif value.lower() == 'false':
+                         value = False
+                    user_inputs[key.lower()] = value
+     return user_inputs
+
+user_inputs_file_path = 'user_inputs.txt'
+user_inputs = read_user_inputs(user_inputs_file_path)
+
+nameInput = user_inputs.get('name', '')
+print(f'name is set to {nameInput}')
+usernameInput = user_inputs.get('username', '')
+print(f'username is set to {usernameInput}')
+emailInput = user_inputs.get('email', None)
+print(f'email is set to {emailInput}')
+archiveAsScreenshotInput = user_inputs.get('archiveasscreenshot', True)
+print(f'archiveAsScreenshot is set to {archiveAsScreenshotInput}')
+archiveAsPDFInput = user_inputs.get('archiveaspdf', True)
+print(f'archiveAsPDF is set to {archiveAsPDFInput}')
+archiveAsWaybackMachineInput = user_inputs.get('archiveaswaybackmachine', False)
+print(f'archiveAsWaybackMachine is set to {archiveAsWaybackMachineInput}')
+isPrivateInput = user_inputs.get('isprivate', False)
+print(f'isPrivate is set to {isPrivateInput}')
+
+date = datetime.now().astimezone(timezone.utc).isoformat(timespec='milliseconds').replace('+00:00', 'Z')
+print(f'date is set to {date}')
+
+@dataclass
+class Tag:
+    createdAt: str = date
+    name: str = ''
+    ownerId: int = 1
+    updatedAt: str = date
+
+@dataclass
+class Link:
+    collectionId: int = 0
+    createdAt: str = date
+    description: str = ''
+    image: str = None
+    lastPreserved: str = date
+    name: str = ''
+    pdf: str = None
+    preview: str = None
+    readable: str = None
+    tags: list[Tag] = field(default_factory=list)
+    textContent: str = None
+    type: str = 'url'
+    updatedAt: str = date
+    url: str = ''
+    
+@dataclass
+class Collection:
+    color: str = '#0ea5e9'
+    createdAt: str = date
+    description: str = ''
+    isPublic: bool = False
+    links: list[Link] = field(default_factory=list)
+    name: str = ''
+    ownerId: int = 1
+    updatedAt: str = date
+
+@dataclass
+class BackupJsonObject:
+    name: str = nameInput
+    username: str = usernameInput
+    email: str = emailInput
+    emailVerified: str = None
+    image: str = None
+    archiveAsScreenshot: bool = archiveAsScreenshotInput
+    archiveAsPDF: bool = archiveAsPDFInput
+    archiveAsWaybackMachine: bool = archiveAsWaybackMachineInput
+    isPrivate: bool = isPrivateInput
+    createdAt: str = date
+    updatedAt: str = date
+    collections: list[Collection] = field(default_factory=list)
+
+linkaceFilename = 'LinkAce_export.csv'
+
+backupObject = BackupJsonObject(name=nameInput, username=usernameInput, email=emailInput, collections=[])
+
+lists: str = ['Unorganized']
+newCollections: list[Collection] = [Collection(name='Unorganized')]
+
+#row[0] - id, row[1] - user_id, row[2] - url, row[3] - title, row[4] - description, row[5] - icon, row[6] - thumbnail, row[7] - is_private, row[8] - status, row[9] - check_disabled, row[10] - created_at, row[11] - updated_at, row[12] - deleted_at, row[13] - tags, row[14] - lists
+with open(linkaceFilename, newline='', encoding='utf8') as csvfile:
+     csv_reader = csv.reader(csvfile, delimiter=',')
+
+     line_count = 0
+
+     for row in csv_reader:
+          if line_count == 0:
+               #print(f'Column names are {", ".join(row)}')
+               line_count += 1
+          else:
+               lastCollectionId = 1
+               if (row[14] != ''):
+                    listNamesFromRow = row[14].split(',')
+                    for listName in listNamesFromRow:
+                         if listName not in lists:
+                              lists.append(listName)
+                              newCollections.append(Collection(name=listName))
+                              lastCollectionId = len(lists)
+
+               tagsFromRow = row[13].split(',')
+               newTags = []
+               for tag in tagsFromRow:
+                    newTags.append(Tag(name=tag))
+            
+               link = Link(collectionId=lastCollectionId, description=row[4], name=row[3], tags=newTags, textContent=None, url=row[2])
+
+               tagsFromRow = row[13].split(',')
+               newTags = []
+               for tag in tagsFromRow:
+                    newTags.append(Tag(name=tag))
+
+               link = Link(collectionId=lastCollectionId, description=row[4], name=row[3], tags=newTags, type='url', url=row[2])
+               newCollections[lastCollectionId - 1].links.append(link)
+
+               line_count += 1
+
+     print(f'Processed the header line and {line_count - 1} link lines.')
+
+backupObject.collections = newCollections
+
+#jsonString = json.dumps(backupObject, default=lambda o: o.__dict__, indent=4)
+#print(f'JSON string: {jsonString}')
+
+filename = 'backup.json'
+
+with open(filename, 'w', encoding='utf-8') as json_file:
+    json.dump(backupObject, json_file, default=lambda o: o.__dict__, indent=4)#Also works here
+    #json_file.write(jsonString)
+
+print(f'Backup exported to {filename}')
+
